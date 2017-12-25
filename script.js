f6908e6f0f40da47ef4c307b300cff24ec3df175
thmsdnnr <thmsdnnr@gmail.com>
2017-12-24 19:30:44 -0600

    outline without programmatic hooks

---
+++ script.js
@@ -0,0 +1,61 @@
+const hOpacity = (opacity) => `data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8' standalone='no'%3F%3E%3Csvg xmlns:dc='http://purl.org/dc/elements/1.1/' xmlns:cc='http://creativecommons.org/ns%23' xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns%23' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/2000/svg' xmlns:sodipodi='http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd' xmlns:inkscape='http://www.inkscape.org/namespaces/inkscape' id='SVGRoot' version='1.1' viewBox='0 0 6 32' height='32' width='6' sodipodi:docname='crystal_g_v.svg' inkscape:version='0.92.2 5c3e80d, 2017-08-06'%3E%3Csodipodi:namedview pagecolor='%23ffffff' bordercolor='%23666666' borderopacity='1' objecttolerance='10' gridtolerance='10' guidetolerance='10' inkscape:pageopacity='0' inkscape:pageshadow='2' inkscape:window-width='1081' inkscape:window-height='674' id='namedview15' showgrid='false' fit-margin-top='0' fit-margin-left='0' fit-margin-right='0' fit-margin-bottom='0' inkscape:zoom='10.072607' inkscape:cx='3.152543' inkscape:cy='16.083225' inkscape:window-x='257' inkscape:window-y='0' inkscape:window-maximized='0' inkscape:current-layer='SVGRoot' /%3E%3Cdefs id='defs847' /%3E%3Cmetadata id='metadata850'%3E%3Crdf:RDF%3E%3Ccc:Work rdf:about=''%3E%3Cdc:format%3Eimage/svg+xml%3C/dc:format%3E%3Cdc:type rdf:resource='http://purl.org/dc/dcmitype/StillImage' /%3E%3Cdc:title%3E%3C/dc:title%3E%3C/cc:Work%3E%3C/rdf:RDF%3E%3C/metadata%3E%3Cg transform='rotate%2890,9.4791781,9.3404682%29' id='layer1'%3E%3Cg transform='matrix%280.99221241,-0.00896014,0.0090301,0.99995923,0.10703934,0.14338106%29' id='g5153'%3E%3Cg id='g5144' transform='rotate%28-0.3,15.92467,15.774108%29'%3E%3Cpath id='path5097' d='M -0.12733556,15.664284 4.999128,12.666096 Z' style='fill:none;stroke:%23000000;stroke-width:0.5857529px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1.0' inkscape:connector-curvature='0' /%3E%3Cpath style='fill:none;stroke:%23000000;stroke-width:0.59571022px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' d='M -0.12805125,15.335377 5.2530513,18.289623 Z' id='path5099' inkscape:connector-curvature='0' /%3E%3Cpath style='fill:none;stroke:%23000000;stroke-width:0.5877521px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' d='M 31.986621,16.028157 26.867163,13.005336 Z' id='path5111' inkscape:connector-curvature='0' /%3E%3Cpath id='path5113' d='M 31.994342,15.710377 26.61324,18.664623 Z' style='fill:none;stroke:%23000000;stroke-width:0.59571022px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3Cpath id='path5115' d='m 4.999128,12.666096 21.868035,0.33924 z' style='fill:none;stroke:%23000000;stroke-width:0.61400002;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:8.10000038;stroke-dasharray:none;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3Cpath id='path5117' d='m 5.2530513,18.289623 c 0,0 21.3601887,0.375 21.3601887,0.375 z' style='fill:none;stroke:%23000000;stroke-width:0.61400002;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3C/g%3E%3C/g%3E%3Cpath transform='translate%28-0.02774072,12.514559%29' id='path5187' d='M 3.8152407,4.603988 C 2.7839907,4.048999 1.7620907,3.5017812 1.5443518,3.3879485 L 1.1484629,3.1809799 3.248262,1.9053867 5.3480612,0.62979357 l 10.7960898,0.0747823 10.79609,0.0747823 1.96875,1.12983903 c 1.082812,0.6214115 1.96875,1.16905 1.96875,1.2169744 0,0.047924 -0.950073,0.6282994 -2.111272,1.2897218 L 26.655198,5.6184799 16.172719,5.6157694 5.6902407,5.613059 Z' style='fill:%2300ff00;fill-opacity:${opacity};stroke:%23000000;stroke-width:0.07675;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:8.10000038;stroke-dasharray:none;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3C/g%3E%3C/svg%3E`;
+const vOpacity = (opacity) => `data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8' standalone='no'%3F%3E%3Csvg xmlns:dc='http://purl.org/dc/elements/1.1/' xmlns:cc='http://creativecommons.org/ns%23' xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns%23' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/2000/svg' xmlns:sodipodi='http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd' xmlns:inkscape='http://www.inkscape.org/namespaces/inkscape' id='SVGRoot' version='1.1' viewBox='0 0 32 6' height='6' width='32' sodipodi:docname='crystal_g_h.svg' inkscape:version='0.92.2 5c3e80d, 2017-08-06'%3E%3Csodipodi:namedview pagecolor='%23ffffff' bordercolor='%23666666' borderopacity='1' objecttolerance='10' gridtolerance='10' guidetolerance='10' inkscape:pageopacity='0' inkscape:pageshadow='2' inkscape:window-width='1081' inkscape:window-height='674' id='namedview15' showgrid='false' fit-margin-top='0' fit-margin-left='0' fit-margin-right='0' fit-margin-bottom='0' inkscape:zoom='10.072607' inkscape:cx='16.083225' inkscape:cy='3.1525438' inkscape:window-x='257' inkscape:window-y='0' inkscape:window-maximized='0' inkscape:current-layer='SVGRoot' /%3E%3Cdefs id='defs847' /%3E%3Cmetadata id='metadata850'%3E%3Crdf:RDF%3E%3Ccc:Work rdf:about=''%3E%3Cdc:format%3Eimage/svg+xml%3C/dc:format%3E%3Cdc:type rdf:resource='http://purl.org/dc/dcmitype/StillImage' /%3E%3Cdc:title%3E%3C/dc:title%3E%3C/cc:Work%3E%3C/rdf:RDF%3E%3C/metadata%3E%3Cg transform='translate%280.02774072,-12.819647%29' id='layer1'%3E%3Cg transform='matrix%280.99221241,-0.00896014,0.0090301,0.99995923,0.10703934,0.14338106%29' id='g5153'%3E%3Cg id='g5144' transform='rotate%28-0.3,15.92467,15.774108%29'%3E%3Cpath id='path5097' d='M -0.12733556,15.664284 4.999128,12.666096 Z' style='fill:none;stroke:%23000000;stroke-width:0.5857529px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3Cpath style='fill:none;stroke:%23000000;stroke-width:0.59571022px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' d='M -0.12805125,15.335377 5.2530513,18.289623 Z' id='path5099' inkscape:connector-curvature='0' /%3E%3Cpath style='fill:none;stroke:%23000000;stroke-width:0.5877521px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' d='M 31.986621,16.028157 26.867163,13.005336 Z' id='path5111' inkscape:connector-curvature='0' /%3E%3Cpath id='path5113' d='M 31.994342,15.710377 26.61324,18.664623 Z' style='fill:none;stroke:%23000000;stroke-width:0.59571022px;stroke-linecap:round;stroke-linejoin:miter;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3Cpath id='path5115' d='m 4.999128,12.666096 21.868035,0.33924 z' style='fill:none;stroke:%23000000;stroke-width:0.61400002;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:8.10000038;stroke-dasharray:none;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3Cpath id='path5117' d='m 5.2530513,18.289623 c 0,0 21.3601887,0.375 21.3601887,0.375 z' style='fill:none;stroke:%23000000;stroke-width:0.61400002;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3C/g%3E%3C/g%3E%3Cpath transform='translate%28-0.02774072,12.514559%29' id='path5187' d='M 3.8152407,4.603988 C 2.7839907,4.048999 1.7620907,3.5017812 1.5443518,3.3879485 L 1.1484629,3.1809799 3.248262,1.9053867 5.3480612,0.62979357 l 10.7960898,0.0747823 10.79609,0.0747823 1.96875,1.12983903 c 1.082812,0.6214115 1.96875,1.16905 1.96875,1.2169744 0,0.047924 -0.950073,0.6282994 -2.111272,1.2897218 L 26.655198,5.6184799 16.172719,5.6157694 5.6902407,5.613059 Z' style='fill:%2300ff00;fill-opacity:${opacity};stroke:%23000000;stroke-width:0.07675;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:8.10000038;stroke-dasharray:none;stroke-opacity:1' inkscape:connector-curvature='0' /%3E%3C/g%3E%3C/svg%3E%0A`
+const digitGenerator = (id) => {
+    const C=document.getElementById('container');
+    let digit=document.createElement('div');
+    digit.id='d_'+id;
+    digit.classList.add('digit');
+    //top
+let boxTop=document.createElement('div');
+boxTop.id='box-top';
+let g_0=document.createElement('div');
+let g_1=document.createElement('div');
+let g_2=document.createElement('div');
+let g_3=document.createElement('div');
+g_0.classList.add('grain-H', 'H-top');
+g_0.id='d_'+id+'_0';
+
+g_1.classList.add('grain-V', 'V-top-left');
+g_1.id='d_'+id+'_1';
+
+g_2.classList.add('grain-V', 'V-top-right');
+g_2.id='d_'+id+'_2';
+
+g_3.classList.add('grain-H', 'H-mid');
+g_3.id='d_'+id+'_3';
+
+boxTop.appendChild(g_0);
+boxTop.appendChild(g_1);
+boxTop.appendChild(g_2);
+boxTop.appendChild(g_3);
+
+digit.appendChild(boxTop);
+//bottom
+let boxBottom=document.createElement('div');
+boxBottom.id='box-bottom';
+let g_4=document.createElement('div');
+let g_5=document.createElement('div');
+let g_6=document.createElement('div');
+
+g_4.classList.add('grain-V', 'V-bottom-left');
+g_4.id='d_'+id+'_4';
+
+g_5.classList.add('grain-H', 'H-bottom');
+g_5.id='d_'+id+'_5';
+
+g_6.classList.add('grain-V', 'V-bottom-right');
+g_6.id='d_'+id+'_6';
+
+boxBottom.appendChild(g_4);
+boxBottom.appendChild(g_5);
+boxBottom.appendChild(g_6);
+digit.appendChild(boxBottom);
+
+C.appendChild(digit);
+}
+
+window.onload = function() {
+    document.getElementById('test').style.backgroundImage = `url("${vOpacity(0.4)}")`;
+    document.getElementById('test2').style.backgroundImage = `url("${hOpacity(0.4)}")`;
+    digitGenerator(0);
+}
